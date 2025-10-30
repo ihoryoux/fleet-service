2c352907aa3158e8d4193fa9559c23929680694e
Ihor <ihor@MacBook-Pro-Ihor.local>
2025-10-30 14:19:31 +0200

    remove comment code

--- src/Repository/FleetSetRepository.php
+++ src/Repository/FleetSetRepository.php
@@ -15,29 +15,4 @@ class FleetSetRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, FleetSet::class);
     }
-
-    //    /**
-    //     * @return FleetSet[] Returns an array of FleetSet objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('f')
-    //            ->andWhere('f.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('f.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?FleetSet
-    //    {
-    //        return $this->createQueryBuilder('f')
-    //            ->andWhere('f.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
